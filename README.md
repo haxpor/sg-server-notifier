def96fac82dd1e096eac92efe29a4a4504add437
Wasin Thonkaew <haxpor@gmail.com>
2017-06-10 17:17:08 +0800

    Got core functionality working

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # sg-server-notifier
-===
+
+
+# Setup
+
+Add following environment variable to your system/server by any mean (recommended via `export`)
+
+`SGSERVER_WEBHOOK=xxxx` in which xxxx is your webhook you got from Slack for your new integration with it.
+
+Then just execute
+
+`node index.js &` (preferrably `node index.js > /dev/null 2>&1 &`
+
+It will check for critical processes if they are down then it will notify a proper message according to that process to slack.
+
+# License
+
+[GNU General Public License v3.0](https://github.com/haxpor/sg-server-notifier/blob/master/LICENSE), Wasin Thonkaew
---
+++ index.js
@@ -0,0 +1,82 @@
+var SlackNode = require('slack-node');
+var exec = require('child_process').exec;
+
+let alertLevel_Normal = 1;
+let alertLevel_High = 2;
+let alertLevel_Critical = 3;
+
+let kChannel = '#sg-server';
+let kBotUsername = 'sg_server_bot';
+
+/**
+ * Execute command with specified callback
+ * @param  {String}   command  Command string
+ * @param  {Function} callback callback function(output)
+ */
+function execute(command, callback) {
+	exec(command, function(error, stdout, stderr) { callback(stdout); });
+};
+
+/**
+ * Notify to slack with level of alert.
+ * @param  {String} message    Message to send to slack
+ * @param  {Number} alertLevel Alert level. 1-3
+ */
+function notifySlack(message, alertLevel) {
+	slack.webhook({
+		channel: kChannel,
+		username: kBotUsername,
+		text: message + "\nLevel: *" + getAlertLevelName(alertLevel) + "*"
+	}, function(err, response) {
+		if (err == null) {
+			console.log("notified message to slack successfully");
+			console.log("---");
+			console.log(message + " with level " + alertLevel);
+			console.log("---");
+		}
+		else {
+			console.log("---");
+			console.log("notified message to slack failed");
+			console.log("---");
+		}
+	});
+}
+
+/**
+ * Return alert level name from level number.
+ * @param  {Number} level Level number
+ * @return {String}       Level number name
+ */
+function getAlertLevelName(level) {
+	if (level == 1) {
+		return "Normal";
+	}
+	else if (level == 2) {
+		return "High";
+	}
+	else if (level == 3) {
+		return "Critical";
+	}
+}
+
+// get webhook from env variable
+let webhookUrl = process.env.SGSERVER_WEBHOOK;
+// create slack
+let slack = new SlackNode();
+slack.setWebhook(webhookUrl);
+
+// set good interval, not too short!!
+// in ms
+let waitInterval = 5000;//600000;
+
+var intetvalID = setInterval(function() {
+
+	// # check if port 80 is listening at the moment or not
+	execute("lsof -i :80", function(output) {
+		if (output == null || output === "") {
+			// process die, notify slack
+			notifySlack("`http` process is down (port `80`)", 3);
+		}
+	});
+
+}, waitInterval);
